be58eb7f1dd0056b3ee650d83ed7f07c53c0326e
richie1988 <richiessikaonga@gmail.com>
2023-10-02 19:30:43 +0200

    Dispatcher and bookslice in the books

--- src/components/book.js
+++ src/components/book.js
@@ -1,34 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 function Book({ book, onDelete }) {
-  const [isDeleted, setIsDeleted] = useState(false);
-
-  const handleDelete = () => {
-    setIsDeleted(true);
-    onDelete();
-  };
-
   return (
     <div>
-      {isDeleted ? (
-        <p>This book has been deleted.</p>
-      ) : (
-        <>
-          <h3>{book.title}</h3>
-          <p>
-            Author:
-            {book.author}
-          </p>
-          <button type="button" onClick={handleDelete}>Delete</button>
-        </>
-      )}
+      <h3>{book.title}</h3>
+      <p>
+        Author:
+        {' '}
+        {book.author}
+      </p>
+      <button type="button" onClick={() => onDelete(book.id)}>Delete</button>
     </div>
   );
 }
 
 Book.propTypes = {
   book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
--- src/pages/books.js
+++ src/pages/books.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import BookList from '../components/bookList';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../redux/books/bookSlice';
+import BookDisplay from '../components/booksDisplay';
 import BookForm from '../components/bookForm';
 
 function Books() {
-  const [books, setBooks] = useState([
-    { id: 1, title: 'Book 1', author: 'Author 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2' },
-  ]);
-
-  const handleDeleteBook = (bookId) => {
-    const updatedBooks = books.filter((book) => book.id !== bookId);
-    setBooks(updatedBooks);
-  };
+  const books = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
 
   const handleAddBook = (newBook) => {
-    setBooks([...books, { ...newBook, id: Date.now() }]);
+    dispatch(addBook(newBook));
+  };
+
+  const handleDeleteBook = (bookIndex) => {
+    dispatch(removeBook(bookIndex));
   };
 
   return (
     <div>
       <h2>Books</h2>
       <BookForm onAdd={handleAddBook} />
-      <BookList books={books} onDelete={handleDeleteBook} />
+      <BookDisplay books={books} onDelete={handleDeleteBook} />
     </div>
   );
 }
