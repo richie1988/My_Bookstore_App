88f1f23edb770956229664c6af95ebb9def46ff8
richie1988 <richiessikaonga@gmail.com>
2023-10-02 15:51:32 +0200

    created the redux toolkit

--- src/components/bookForm.js
+++ src/components/bookForm.js
@@ -2,22 +2,18 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function BookForm({ onAdd }) {
-  // State for tracking the new book's title and author
   const [newBook, setNewBook] = useState({ title: '', author: '' });
 
   // Handle changes in the input fields
   const handleChange = (e) => {
     const { name, value } = e.target;
-    // Update the corresponding field in the newBook state
     setNewBook({ ...newBook, [name]: value });
   };
 
   // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Call the provided onAdd function with the new book data
     onAdd(newBook);
-    // Reset the input fields
     setNewBook({ title: '', author: '' });
   };
 
@@ -51,7 +47,6 @@ function BookForm({ onAdd }) {
   );
 }
 
-// PropTypes definition to validate the onAdd prop
 BookForm.propTypes = {
   onAdd: PropTypes.func.isRequired,
 };
--- src/pages/books.js
+++ src/pages/books.js
@@ -9,13 +9,11 @@ function Books() {
   ]);
 
   const handleDeleteBook = (bookId) => {
-    // Remove the book from the state (you can also send a DELETE request to an API).
     const updatedBooks = books.filter((book) => book.id !== bookId);
     setBooks(updatedBooks);
   };
 
   const handleAddBook = (newBook) => {
-    // Add a new book to the state (you can also send a POST request to an API).
     setBooks([...books, { ...newBook, id: Date.now() }]);
   };
 
