fc104ee1f97f26f7f72aa545619dc444d0273376
richie1988 <richiessikaonga@gmail.com>
2023-10-06 17:50:52 +0200

    Styled the Bookstore App

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import {
   BrowserRouter, Route, Routes,
 } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import BookList from './components/bookList';
-import NavLinks from './components/NavLink';
 import Categories from './components/Categories';
 
-const App = () => (
-  <BrowserRouter>
-    <NavLinks />
-    <Routes>
-      <Route path="/" element={<BookList />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- src/components/books.js
+++ src/components/books.js
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { removeBookAsync } from '../redux/books/bookSlice';
+import './books.css';
 
 const Book = ({ book }) => {
   const {
     itemId, title, author, category,
   } = book;
 
-  const progress = Math.floor(Math.random() * 101);
+  const [progress, setProgress] = useState(Math.floor(Math.random() * 101));
+  const [currentPage, setCurrentPage] = useState(Math.floor(Math.random() * 301));
+
   const dispatch = useDispatch();
 
   const handleRemove = async () => {
     dispatch(removeBookAsync(itemId));
   };
 
+  const handleUpdateProgress = () => {
+    const newProgress = Math.floor(Math.random() * 101);
+    const newPage = Math.floor(Math.random() * 301);
+    setProgress(newProgress);
+    setCurrentPage(newPage);
+  };
+
   return (
     <div className="book-container">
       <div className="book-header">
-        <h3>{category}</h3>
-        <h2>{title}</h2>
-        <p>{author}</p>
+        <h3 className="book-category">{category}</h3>
+        <h2 className="book-title">{title}</h2>
+        <p className="book-author">{author}</p>
         <div className="book-actions">
-          <button type="button">Comments</button>
-          <button type="button" onClick={handleRemove}>Remove</button>
+          <button type="button">Comments |</button>
+          <button type="button" onClick={handleRemove}>Remove |</button>
           <button type="button">Edit</button>
         </div>
       </div>
-      <div className="book-progress">
-        <div>
-          <CircularProgressbar value={progress} />
+      <section className="book-progress">
+        <CircularProgressbar value={progress} text={`${progress}%`} />
+        <span className="completed">Completed</span>
+        |
+        <div className="update-progress-section">
+          <span>Current Page</span>
+          <p>
+            Page:
+            {currentPage}
+          </p>
+          <button type="button" onClick={handleUpdateProgress}>Update Progress</button>
         </div>
-      </div>
-      <div className="vl" />
+      </section>
     </div>
   );
 };
